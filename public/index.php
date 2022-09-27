cf9113a1fcd6cecc12c675277d4ac31afedc4ee5
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-27 10:32:16 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
     include "conn.php";
     if(isset($_SESSION["kakao_member_code"])==false || !$_SESSION['kakao_member_code']){
     ?>
-        <script>
-            location.replace("login.php");
-        </script>
+
     <?php
       exit;
     }
