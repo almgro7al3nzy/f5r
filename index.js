3cd1cdb6a7d6373a31bb5d511a7c36a633a271bc
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-29 05:34:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,40 @@ let numUsers = 0;
 );
 
 
+io.on('connection', (socket) => {
 
+console.log('user connected')
+
+socket.on('join', function(userNickname) {
+
+        console.log(userNickname +" : has joined the chat "  );
+
+        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+    });
+
+
+socket.on('messagedetection', (senderNickname,messageContent) => {
+       
+       //log the message in console 
+
+       console.log(senderNickname+" :" +messageContent)
+        //create a message object 
+       let  message = {"message":messageContent, "senderNickname":senderNickname}
+          // send the message to the client side  
+       io.emit('message', message );
+     
+      });
+      
+  
+ socket.on('disconnect', function() {
+    console.log( ' user has left ')
+    socket.broadcast.emit("userdisconnect"," user has left ") 
+
+});
+
+
+
+});
 
 
 
