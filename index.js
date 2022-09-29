0f84293342be9a1cfca4e8e92b9ef38d90ee21ad
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-29 05:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,38 +8,13 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
+let numUsers = 0;
+
+);
 
 
 
 
-console.log('user connected')
-
-socket.on('join', function(userNickname) {
-
-        console.log(userNickname +" : has joined the chat "  );
-
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
-    });
-
-
-socket.on('messagedetection', (senderNickname,messageContent) => {
-       
-       //log the message in console 
-
-       console.log(senderNickname+" :" +messageContent)
-        //create a message object 
-       let  message = {"message":messageContent, "senderNickname":senderNickname}
-          // send the message to the client side  
-       io.emit('message', message );
-     
-      });
-      
-  
- socket.on('disconnect', function() {
-    console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") 
-
-});
 
 
 server.listen(app.get('port'), function(){
