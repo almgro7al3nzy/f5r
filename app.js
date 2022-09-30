15bb82d13b5163ff65994d050215a22f21f75127
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-30 17:45:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
 
 const general = io.of("/general");
 const football = io.of("/football");
