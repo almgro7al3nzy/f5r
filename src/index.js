f9f0887eb0992a6370bb26033ff20adf7ae52e05
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 05:24:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,31 +5,13 @@ app.use(cors());
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 require("dotenv/config");
-const general = io.of("/general");
-const football = io.of("/football");
-const basketball0 = io.of("/1");
-const basketball21 = io.of("/2");
-const basketball32 = io.of("/3");
-const basketball564 = io.of("/4");
-const basketball56 = io.of("/5");
-const basketballsa = io.of("/6");
-const basketballdf = io.of("/7");
-const basketballgfh = io.of("/8");
-const baskhjetball = io.of("/9");
-const basketxcbball = io.of("/10");
-const baskfgdetball = io.of("/11");
-const baskedvbtball = io.of("/12");
-const basketvbball = io.of("/13");
-const baskevtball = io.of("/14");
-const baskebtball = io.of("/15");
-const basketxcbvball = io.of("/16");
-const basketbxcbvall = io.of("/17");
-const rooxcvbcmfdg = io.of("/18");
-const basketbxcvball = io.of("/19");
-const basketxcvbball = io.of("/20");
-const basketxcvbball = io.of("/21");
+const general = io.of("/1"+"/2"+"/3"+"/4"+"/5"+);
+
 var people = {};
 
+var generalTotalUser = 0;
+var footballTotalUser = 0;
+var basketballTotalUser = 0;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/templates/index.html");
 });
