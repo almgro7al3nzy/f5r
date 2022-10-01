e3cabc059e5cce0bfd1a39ec597ac86bfc482508
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 05:14:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,19 @@
 const app = require("express")();
 const cors = require("cors");
 app.use(cors());
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+require("dotenv/config");
 const general = io.of("/general");
 const football = io.of("/football");
 const basketball = io.of("/1");
-const basketball = io.of("/2");
-const basketball = io.of("/3");
-const basketball = io.of("/4");
-const basketball = io.of("/5");
-const basketball = io.of("/6");
-const basketball = io.of("/7");
-const basketball = io.of("/8");
-const basketball = io.of("/9");
-const basketball = io.of("/10");
-const basketball = io.of("/11");
-const basketball = io.of("/12");
-const basketball = io.of("/13");
-const basketball = io.of("/14");
-const basketball = io.of("/15");
-const basketball = io.of("/16");
-const basketball = io.of("/17");
-const room-id = io.of("/18");
-const basketball = io.of("/19");
-const basketball = io.of("/20");
-const basketball = io.of("/21");
+
 var people = {};
 
 var generalTotalUser = 0;
 var footballTotalUser = 0;
 var basketballTotalUser = 0;
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-require("dotenv/config");
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/templates/index.html");
 });
