269aa523e250c491a90856bf5cbc878ed61bef71
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 05:21:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,34 @@ const io = require("socket.io")(server);
 require("dotenv/config");
 const general = io.of("/general");
 const football = io.of("/football");
-const basketball = io.of("/1");
-
+const basketball0 = io.of("/1");
+const basketball21 = io.of("/2");
+const basketball32 = io.of("/3");
+const basketball564 = io.of("/4");
+const basketball56 = io.of("/5");
+const basketballsa = io.of("/6");
+const basketballdf = io.of("/7");
+const basketballgfh = io.of("/8");
+const baskhjetball = io.of("/9");
+const basketxcbball = io.of("/10");
+const baskfgdetball = io.of("/11");
+const baskedvbtball = io.of("/12");
+const basketvbball = io.of("/13");
+const baskevtball = io.of("/14");
+const baskebtball = io.of("/15");
+const basketxcbvball = io.of("/16");
+const basketbxcbvall = io.of("/17");
+const rooxcvbcmfdg = io.of("/18");
+const basketbxcvball = io.of("/19");
+const basketxcvbball = io.of("/20");
+const basketxcvbball = io.of("/21");
 var people = {};
 
-var generalTotalUser = 0;
-var footballTotalUser = 0;
-var basketballTotalUser = 0;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/templates/index.html");
 });
 
 io.on("connection", (client) => {
-    socket.on('join', function(msg){
-        footballTotalUser = footballTotalUser + 1;
-        console.log(nickname + ": has joined to general channel");
-        console.log("channel user count:" + footballTotalUser);
-        socket.broadcast.emit('join', {nickname: nickname, count: footballTotalUser});
-        socket.emit('activeUser', {count: footballTotalUser});
-    });
 
   client.on("join-room", ({ roomId, name}) => {
     if (roomId) {
