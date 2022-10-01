1470aa72fa907a692f1c4645e2d06ff73d05ecaa
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 05:32:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ const { ExpressPeerServer } = require('peer');
 const schedule = require('node-schedule');
 const controlRooms = require("./controllers/controlRooms"); 
 
-const twilioObj = {
-    username : null,
-    cred : null 
-}
-
-// Voice chat uses turn server, not required locally 
-if(process.env.USE_TWILIO==="yes") { 
-    const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    client.tokens.create().then(token => {
-        twilioObj.username = token.username;
-        twilioObj.cred = token.password; 
-    });
-
-    //every 12 hours 
-    schedule.scheduleJob("*/12 * * *",()=>{
-        console.log("CRON running"); 
-        const client = require('twilio')(process.env.accountSid, process.env.authToken);
-        client.tokens.create().then(token => {
-            twilioObj.username = token.username;
-            twilioObj.cred = token.password; 
-        });
-        controlRooms.deQRoom();
-    })
-}
-
-const cors = require('cors');
-const app = express(); 
-
-const router = require("./controllers/chatController");
-const server = http.createServer(app);
-const io = socketio(server); 
-
 const general = io.of("/general");
 const football = io.of("/football");
 const basketball = io.of("/basketball");
@@ -125,7 +93,6 @@ basketball.on('connection', function (socket) {
         socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
     });
 });
-
 
 
 const PORT = process.env.PORT || 5000; 
